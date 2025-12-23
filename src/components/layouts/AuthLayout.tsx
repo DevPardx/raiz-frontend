2ba38dcae92f28a6d3dfb6220015d3439ff999ab
DevPardx <engineer.pardo@gmail.com>
2025-12-23 11:45:59 -0600

    fix: render propper logo based on the theme

--- src/components/layouts/AuthLayout.tsx
+++ src/components/layouts/AuthLayout.tsx
@@ -3,10 +3,8 @@ import type { ReactNode } from "react";
 export default function AuthLayout({ children }: { children: ReactNode }) {
     return (
         <div className="h-full flex flex-col items-center justify-center px-4">
-            <picture>
-                <source srcSet="/svg/logo-light.svg" media="(prefers-color-scheme: dark)" className="size-32 pointer-events-none"/>
-                <img src="/svg/logo-dark.svg" alt="Raiz Logo" className="size-32 pointer-events-none"/>
-            </picture>
+            <img src="/svg/logo-dark.svg" alt="Raiz Logo" className="size-32 pointer-events-none dark:hidden"/>
+            <img src="/svg/logo-light.svg" alt="Raiz Logo" className="size-32 pointer-events-none hidden dark:block"/>
             { children }
         </div>
     );
